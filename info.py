646001d1f49de108b49404efa451051a332f5342
priyangania010-collab <priyangania010@gmail.com>
2025-09-18 15:29:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ FILES_LIMIT = int(environ.get("FREE_FILES", "3")) #No. of File User Gets In Free
 
 # Database Settings
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://pawanivimasha8:20030928@autofilter.jdtmx.mongodb.net/?retryWrites=true&w=majority&appName=AutoFilter")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://dhyeyrepo_db_user:L1sf77qxgHcx5OoM@repo.v4hrlux.mongodb.net/?retryWrites=true&w=majority&appName=repo")
 FILES_DATABASE_URL = environ.get('FILES_DATABASE_URL', "mongodb+srv://flexbeat616:20030928@vip.tj7x0v1.mongodb.net/?retryWrites=true&w=majority&appName=vip") # For Files
 SECOND_FILES_DATABASE_URL = environ.get('SECOND_FILES_DATABASE_URL', "mongodb+srv://minideveloper06:nvRrHLViR4j7DALy@db2.q5mql.mongodb.net/?retryWrites=true&w=majority&appName=db2") # 2nd DB For Files
 DATABASE_NAME = environ.get('DATABASE_NAME', "Silicon")
